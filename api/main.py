2e955eb921bd224717f3f42f406480d76e75c893
snyamson <snyamson@st.knust.edu.gh>
2024-06-17 08:54:22 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = FastAPI()
 
 @app.get('/')
 def hello_world():
-    return "Hello,World"
+    return "Hello,World - Main"
+
+
+# App server setup
+if os.getenv("ENVIRONMENT") == "development":
+    if __name__ == "__main__":
+        import uvicorn
+        uvicorn.run(app)
